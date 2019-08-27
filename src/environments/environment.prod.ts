62176be6c2e58925529d45a5c4c66b9cfd8d2b8f
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-26 22:39:16 -0700

    Added Bootstrap and defining routes
    -) Separate Navbar
    -) Components Added:
       1) home
       2) login
       3) products
       4) shopping-cart
       5) check-out
       6) order-success
       7) my-orders
       8) admin/admin-products
       9) admin/admin-orders

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBj3lcANJJN3w9QnQCPWDPxIwpVT0Ng87I",
+    authDomain: "angular-oshop-project.firebaseapp.com",
+    databaseURL: "https://angular-oshop-project.firebaseio.com",
+    projectId: "angular-oshop-project",
+    storageBucket: "",
+    messagingSenderId: "396227673051",
+    appId: "1:396227673051:web:18632f33909e3118"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBj3lcANJJN3w9QnQCPWDPxIwpVT0Ng87I",
+    authDomain: "angular-oshop-project.firebaseapp.com",
+    databaseURL: "https://angular-oshop-project.firebaseio.com",
+    projectId: "angular-oshop-project",
+    storageBucket: "",
+    messagingSenderId: "396227673051",
+    appId: "1:396227673051:web:18632f33909e3118"
+  }
 };
 
 /*
