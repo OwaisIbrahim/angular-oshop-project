5c234194bf9f89ceaa17612101a173f87df33057
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-08-27 12:35:55 -0700

    Google Auth Added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) {
   }
 
-
+  login() {
+    // 1. Enable Google Signin in sign in method on firebase console
+    // 2. Add the following line 
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
   
 }
