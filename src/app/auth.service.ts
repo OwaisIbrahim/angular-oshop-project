7729de0ebdf5884ae0e7d99ebb3e1ae491b01913
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-01 01:45:38 -0700

    Protecting the routes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth'
 import * as firebase from 'firebase';
@@ -11,7 +12,7 @@ export class AuthService {
   //where $ is just an notation to describe developers that its an observable
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth,) {
     this.user$ = afAuth.authState;
     console.log(this.user$);
 
