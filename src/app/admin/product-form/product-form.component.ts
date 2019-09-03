7e76dafde2c16021becd9d1f350947a96f85e66f
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-03 06:16:37 -0700

    Add new Product Form Added   - Service Created:     1) ng g s category   - Categories Dropdown Added

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,13 +1,17 @@
+import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component({ 
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
+  categories$;
 
-  constructor() { }
+  constructor(categoryService: CategoryService) {
+    this.categories$ = categoryService.getCategories();
+   }
 
   ngOnInit() {
   }
