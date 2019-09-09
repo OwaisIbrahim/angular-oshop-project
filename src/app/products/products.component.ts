72a389c598d8de7ce1c950228b3c01c0956481a7
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-09 05:17:27 -0700

    Product Filter Component added with refactor of code

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,5 @@
 import { ActivatedRoute } from '@angular/router';
-import { CategoryService } from './../category.service';
-import { ProductService } from './../product.service';
-import { FirebaseListObservable } from 'angularfire2/database';
+import { ProductService } from './../product.service';  
 import { Product } from '../models/product';
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
@@ -14,13 +12,12 @@ import 'rxjs/add/operator/switchMap';
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  categories$: FirebaseListObservable<any[]>;
   category: string;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService, 
-    categoryService: CategoryService) {
+    productService: ProductService 
+    ) {
     
     productService
       .getAll()
@@ -30,11 +27,12 @@ export class ProductsComponent implements OnInit {
       })
       .subscribe(params => {
         this.category = params.get('category');
+        
         this.filteredProducts = (this.category) ?
-          this.products.filter(p => p.category === this.category):
+          this.products.filter(p => p.category === this.category) :
           this.products
       }); 
-    this.categories$ = categoryService.getAll();
+    
 
     
    }
