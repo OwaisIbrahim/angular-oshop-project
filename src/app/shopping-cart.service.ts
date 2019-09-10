953952a26e6382185d78a0958cea40c23d7313a6
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-10 11:29:17 -0700

    Refactor - using async await

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,6 @@
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import { Product } from './models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,24 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create() {
+  private create() {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
   }
+
+  private getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private async getOrCreateCart() {
+    let cartId = localStorage.getItem('cartId');
+    if(!cartId) {
+      let result = await this.create();
+      localStorage.setItem('cartId', result.key);
+      return this.getCart(result.key);
+    }
+    return this.getCart(cartId);
+  }
+
 }
