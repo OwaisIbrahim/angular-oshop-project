2b741d160cc4566e7cd344535482d2797cf8c23f
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-11 13:09:18 -0700

    Building Shopping Cart Page

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,21 +1,30 @@
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from './../shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';   
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
   
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  constructor(
+    private auth: AuthService,
+    private shoppingCartService: ShoppingCartService) {
    }
 
   logout() {
     this.auth.logout();
   }
-
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    this.cart$ = await this.shoppingCartService.getCart(); 
+  }
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,13 +1,16 @@
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 import { Product } from './models/product';
 import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/map';
+import { ShoppingCart } from './models/shopping-cart';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
-
+  
   constructor(private db: AngularFireDatabase) { }
 
   private create() {
@@ -16,9 +19,10 @@ export class ShoppingCartService {
     });
   }
 
-  async getCart() {
+  async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/' + cartId);
+    return this.db.object('/shopping-carts/' + cartId)
+      .map( (x: any) => new ShoppingCart(x.items) );
   }
 
   private getItem(cartId: string, productId: string) {
@@ -35,11 +39,19 @@ export class ShoppingCartService {
   }
 
   async addToCart(product: Product) {
+    this.updateItemQuantity(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItemQuantity(product, -1);
+  }
+
+  private async updateItemQuantity(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
     item$.take(1).subscribe( (item: any) => {
-      item$.update({ product: product, quantity: (item.quantity || 0) + 1 });
-    })
+      item$.update({ product: product, quantity: (item.quantity || 0) + change });
+    });
   }
 
 }
