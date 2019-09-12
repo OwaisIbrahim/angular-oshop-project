3fdf8227efd12efd9dcca444761cb8537b8c59a2
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-12 12:54:12 -0700

    Order Object placed in firebase

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth.service';
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCart } from '../models/shopping-cart';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy{
+ 
+  shipping = {}; 
+  cart: ShoppingCart;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
+  
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService,
+    private shoppingCartService: ShoppingCartService) {}
 
-  constructor() { }
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
+  } 
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
   }
 
+  placeOrder() {
+    
+    let order = {
+      userId: this.userId,
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+    };
+    
+    this.orderService.storeOrder(order);
+  }    
 }
