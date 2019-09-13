1a051853d6eafe5486f529edd294160631f1ade2
Owais Ibrahim <owais.ibrahim@systemsltd.com>
2019-09-12 22:29:17 -0700

    360 Refactoring Extract ShippingFormComponent - shipping-form, shipping-form-summary component added for checkout wrapping

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,45 +1,23 @@
-import { AuthService } from './../auth.service';
+import { Observable } from 'rxjs/observable'
 import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ShoppingCart } from '../models/shopping-cart';
-import { OrderService } from '../order.service';
-import { Order } from '../models/order';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy{
+export class CheckOutComponent implements OnInit{
  
-  shipping = {}; 
-  cart: any = {};
-  userId: string;
-  cartSubscription: Subscription;
-  userSubscription: Subscription;
+  cart$: Observable<ShoppingCart>;
   
   constructor(
-    private router: Router,
-    private authService: AuthService,
-    private orderService: OrderService,
     private shoppingCartService: ShoppingCartService) {}
 
   async ngOnInit() {
-    let cart$ = await this.shoppingCartService.getCart();
-    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
-    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
+    this.cart$ = await this.shoppingCartService.getCart();
   } 
 
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-    this.userSubscription.unsubscribe();
-  }
-
-  async placeOrder() {
-    let order = new Order(this.userId, this.shipping, this.cart);
-    let result = await this.orderService.placeOrder(order);
-    this.router.navigate(['/order-success', result.key])
-  }    
 }
